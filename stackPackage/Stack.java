cabdb461856137ce0b5c265dfb2b418b8b07f607
Alireza Teimoori <s445969@ocsb.local>
2019-03-05 15:02:29 -0500

    feat: create java files

---
+++ stackPackage/Stack.java
@@ -0,0 +1,16 @@
+package stackPackage;
+
+public class Stack {
+
+    public int test;
+
+    public stack(this.test) {
+        
+        this.test = number;
+    }
+
+    public push(userNumber){
+
+        
+    }
+}
