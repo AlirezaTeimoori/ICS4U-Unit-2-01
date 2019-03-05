3ef3ab6ac1e45b1fd51ca8bcf2724d14c4d244d8
Alireza Teimoori <s445969@ocsb.local>
2019-03-05 14:12:15 -0500

    feat: create vb file

---
+++ StackForm.vb
@@ -0,0 +1,27 @@
+﻿Option Strict On
+Option Explicit On
+'Created on: Sept-2009
+'Created by: Mr. Coxall
+'Created for: ICS4U
+'This program uses the MrCoxallStack class
+
+Public Class StackForm
+
+    ' this is the declaration of my stack
+    Dim aStack As MrCoxallStack
+
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+        ' place a value on the stack
+        Dim aNumber As Integer
+
+        aNumber = CInt(Me.txtNumber.Text)
+
+        aStack.Push(aNumber)
+    End Sub
+
+    Private Sub StackForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' instantiat the stack
+        aStack = New MrCoxallStack
+    End Sub
+
+End Class
